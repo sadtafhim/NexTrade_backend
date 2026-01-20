d55825a3bd27cfab1951b94d73498a34981b43e6
Saad <tafhim.sad@gmail.com>
2026-01-20 17:21:06 +0600

    last commit

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
